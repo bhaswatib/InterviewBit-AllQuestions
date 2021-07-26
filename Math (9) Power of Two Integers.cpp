d32e5ce43c8ce7e509ebd5a4c2cfdb7e8aedccdb
Bhaswati Boro <68868881+bhaswatib@users.noreply.github.com>
2021-07-26 22:35:59 +0530

    Create Math (9) Power of Two Integers.cpp

---
+++ Math (9) Power of Two Integers.cpp
@@ -0,0 +1,47 @@
+/*
+Power Of Two Integers
+
+Asked in:  
+Housing
+Amazon
+Given a positive integer which fits in a 32 bit signed integer, find if it can be expressed as A^P where P > 1 and A > 0. A and P both should be integers.
+
+Example
+
+Input : 4
+Output : True  
+as 2^2 = 4. 
+*/
+
+Solution:
+int Solution::isPower(int A) 
+{
+  if(A==0) return 0;
+  if(A==1) return 1;
+  for(int i=2;i*i<=A;i++)
+  {
+    if(A%i==0)
+    {
+      for(int j=0;j*j<=A;j++)
+      {
+        if(pow(i,j)==A) return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
